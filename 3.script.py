0d8d2c51ce402c1bdef9066595a5e6e640920e70
Hemanth Surthani <hemanthsurthani7@gmail.com>
2025-12-07 21:38:55 +0530

    day 3,4,5 code update

---
+++ 3.script.py
@@ -0,0 +1,16 @@
+var = 6
+var2 = 8
+
+
+def adding():
+    print(var + var2)
+
+
+print(5 + 6)
+adding()
+
+# 1. intractive mode - small tasks & testing
+# 2. script - production apps  & automation
+# 3. notbook - data analysis
+# 4. ide mode - profitional development
+# 5. databriks notebook - big data analysis
